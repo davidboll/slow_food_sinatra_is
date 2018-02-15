7815caab277daa18ed06177fdf9a99da282427fa
Aiden_Jubelin <aiden.jubelin@gmail.com>
2018-02-15 15:33:17 +0100

    added user acceptance test and class implementation

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,9 +1,14 @@
 class SlowFoodApp
   get '/login' do
-
+    erb :login
   end
 
   get '/signup' do
 
   end
+
+  get  '/loggedin' do
+    erb :welcome
+  end
+
 end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -1,23 +1,29 @@
 When(/^a customer visits a login page$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  visit '/login'
 end
 
-Then(/^he should see text "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+Then(/^he should see text "([^"]*)"$/) do |text|
+   page.should have_content(text)
 end
 
 Then(/^he should see a form$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  page.has_content?('form')
 end
 
 Then(/^he fills in input field "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
-  pending # Write code here that turns the phrase above into concrete actions
+    fill_in(arg1, :with => arg2)
+    fill_in(arg1, :with => arg2)
+    #fill_in(password, :with => '12345')
 end
 
 Then(/^he clicks a "([^"]*)" button$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+  click_button('login')
 end
 
 Then(/^then he is redirected to "([^"]*)" page$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+   visit('/')
+end
+
+Then(/^show me the page$/) do
+  save_and_open_page
 end
